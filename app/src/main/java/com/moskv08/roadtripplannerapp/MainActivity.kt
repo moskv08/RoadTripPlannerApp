e74ddc28a3bc86b100eb03cf625a26d1cd918740
robbmoskv <fabrizio_39@hotmail.com>
2023-01-08 19:12:57 +0100

    Initial Setup

---
+++ app/src/main/java/com/moskv08/roadtripplannerapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.moskv08.roadtripplannerapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.moskv08.roadtripplannerapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
